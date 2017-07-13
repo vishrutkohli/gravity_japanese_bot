ddcab14b4e4c3925bbe0733ecb7ff58b700f83d9
vishrutkohli <kohlivishrut@gmail.com>
2017-07-13 19:24:00 +0530

    make it better

--- DHLchatbot/urls.py
+++ DHLchatbot/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+import main.views as v
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DHLchatbot.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^facebook_auth/?$',v.MyChatBotView.as_view()),
+    url(r'^index$',v.index),
 
     url(r'^admin/', include(admin.site.urls)),
 )
