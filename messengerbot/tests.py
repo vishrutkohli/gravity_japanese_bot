9da7b42af45713e67c771b6058d0d4df4fa2f27a
icancode23 <ernipunarora@gmail.com>
2017-07-22 18:37:01 +0530

    deleted basic files

--- messengerbot/tests.py
+++ messengerbot/tests.py
@@ -11,6 +11,4 @@ class TestVerifyToken(TestCase):
 		test_string="TestCase"
 		resp=self.client.get("/facebook_auth/?hub.verify_token=dhlchatbot&hub.challenge=%s"%test_string)
 		self.assertEqual(resp.status_code,200)
-		print resp.content
-		print resp.context
-		self.assertEqual(resp.text,test_string)
+		self.assertEqual(resp.content,test_string)
