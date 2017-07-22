51ab279779c0182b9648002978bbe0f02ea44fae
icancode23 <ernipunarora@gmail.com>
2017-07-22 18:23:17 +0530

    Added a basic test

--- messengerbot/tests.py
+++ messengerbot/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+#Testing the facebook verify token end point 
+
+class TestVerifyToken(TestCase):
+
+	## testing for the correct response
+	def test_VerifyToken(self):
+		test_string="TestCase"
+		resp=self.client.get("/facebook_auth/?hub.verify_token=dhlchatbot&hub.challenge=%s"%test_string)
+		self.assertEqual(resp.status_code,200)
+		self.assertEqual(resp.text,test_string)
