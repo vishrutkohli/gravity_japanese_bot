b500e5aa3674f84e137758d2a480a3be45b2d9c7
vishrutkohli <kohlivishrut@gmail.com>
2017-07-24 02:54:55 +0530

    a

--- messengerbot/api_ai.py
+++ messengerbot/api_ai.py
@@ -138,12 +138,12 @@ def database_intercept(context,response ,sender_id):
     order_instance = order.objects.get_or_create(fbid = sender_id)[0]
     
     if context == "order-placing-location-from":
-        print "checking database intercept"  + str(response['result']['contexts'][0]["parameters"]["geo-country"])
-        order_instance.address_to = response['result']['contexts'][0]["parameters"]["geo-country"]
+        print "checking database intercept"  + str(response['result']['contexts'][0]["parameters"]["location"]["country"])
+        order_instance.address_to = response['result']['contexts'][0]["parameters"]["location"]["country"]
         order_instance.save()
     elif context == "box-size":
-        print "checking database intercept"  + str(response['result']['contexts'][0]["parameters"]["geo-country"])
-        order_instance.address_from = response['result']['contexts'][0]["parameters"]["geo-country"]
+        print "checking database intercept"  + str(response['result']['contexts'][0]["parameters"]["location"]["country"])
+        order_instance.address_from = response['result']['contexts'][0]["parameters"]["location"]["country"]
         order_instance.save()
     elif context in ["Envelope-1" ,"Box-2"  , "Box-3" , "Box-4" , "Box-5" , "Box-6" , "Box-7"  ]:
         print "checking database intercept"  + str(context)
