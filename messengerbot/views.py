951a337894ac6dbed8c934c62c3631fe2cc46325
Vishrut Kohli <vishrutkohli@Vishruts-MacBook-Air.local>
2019-09-05 14:43:57 +0900

    first commit

--- messengerbot/views.py
+++ messengerbot/views.py
@@ -47,6 +47,7 @@ def post_facebook_message(fbid,message_text):
     translator = Translator()
     message_text = translator.translate(message_text, dest='ja').text
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+    print("message_text")
     try:
         response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
         status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
