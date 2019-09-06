3218df20d3b7a383cf4e5360cfd33e938cdf298e
Vishrut Kohli <vishrutkohli@Vishruts-MacBook-Air.local>
2019-09-06 11:44:35 +0900

    cards converter added

--- messengerbot/views.py
+++ messengerbot/views.py
@@ -73,7 +73,8 @@ def post_facebook_message(fbid,message_text):
 
     
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
-    print("message_text")
+    print("message_text!!!!!!!")
+    print(message_text)
     try:
         response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
         status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
